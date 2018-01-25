a98b0b7a648590112198eec2539c50ec7f1b0df6
yksoul <978262587@qq.com>
2018-01-26 00:47:14 +0800

    mybatis-generator-plugins

---
+++ src/main/java/com/yksoul/common/Constants.java
@@ -0,0 +1,8 @@
+package com.yksoul.common;
+
+/**
+ * @author yk
+ * @version 1.0 Date: 2018-01-25
+ */
+public class Constants {
+}
---
+++ src/main/java/com/yksoul/plugins/LombokPlugin.java
@@ -0,0 +1,71 @@
+package com.yksoul.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * @author yk
+ * @version 1.0 Date: 2018-01-25
+ */
+public class LombokPlugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> list) {
+        return true;
+    }
+
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        addAnnotation(topLevelClass);
+        return true;
+    }
+
+
+    /**
+     * 返回false,取消生成getter方法
+     *
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * 返回false,取消生成setter方法
+     *
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * 添加注解
+     *
+     * @param topLevelClass
+     */
+    private void addAnnotation(TopLevelClass topLevelClass) {
+        String importType = "lombok.Data";
+        FullyQualifiedJavaType full = new FullyQualifiedJavaType(importType);
+        topLevelClass.addImportedType(importType);
+        topLevelClass.addAnnotation("@Data");
+    }
+}
